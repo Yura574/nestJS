229b5ddba26ac265618806060bb78f41bb05fb60
Yura <yura5742248@gmail.com>
2022-06-18 23:26:32 +0300

    connect db

--- src/appModule.ts
+++ src/appModule.ts
@@ -2,20 +2,28 @@ import {Module} from "@nestjs/common";
 import {AppController} from "./appController";
 import {AppService} from "./appService";
 import {SequelizeModule} from "@nestjs/sequelize";
+import {ConfigModule} from "@nestjs/config";
+import {AuthController} from "./auth/auth.controller";
+import {AuthService} from "./auth/auth.service";
+import {User} from "./user/user.model";
 
 
 @Module({
-    controllers: [AppController],
-    providers: [AppService],
+    controllers: [AppController, AuthController],
+    providers: [AppService, AuthService],
     imports: [
+        ConfigModule.forRoot({
+            envFilePath: '.env'
+            }
+        ),
         SequelizeModule.forRoot({
             dialect: 'postgres',
-            host: 'localhost',
-            port: 5432,
-            username: 'postgres',
-            password: 'unbiliever13',
-            database: 'test_db',
-            models: [],
+            host: process.env.HOST,
+            port: Number(process.env.POSTGRES_PORT),
+            username: process.env.POSTGRES_USERNAME,
+            password: process.env.PASSWORD,
+            database: process.env.DATABASE,
+            models: [User],
             autoLoadModels: true
         }),
     ],

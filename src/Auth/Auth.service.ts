77e92f1b612d4450840eae75c274e70ebb9c0c70
Yura <yura5742248@gmail.com>
2022-06-28 23:27:05 +0300

    add register logic

--- src/Auth/Auth.service.ts
+++ src/Auth/Auth.service.ts
@@ -1,6 +1,7 @@
-import {Injectable} from "@nestjs/common";
+import {ForbiddenException, Injectable} from "@nestjs/common";
 import {PrismaService} from "../Prisma/Prisma.service";
 import * as argon from "argon2";
+import {PrismaClientKnownRequestError} from "@prisma/client/runtime";
 
 
 
@@ -14,21 +15,34 @@ constructor(private prisma: PrismaService) {
         //generate the password hash
         const hash = await argon.hash(authDto.password)
         //save the new user in the db
-        const newUser = await this.prisma.user.create({
-            data:{
-                email: authDto.email,
-                password: hash
-            },
-            select:{
-                id: true,
-                email: true,
-                createdAt: true
+       try{
+           const newUser = await this.prisma.user.create({
+               data:{
+                   email: authDto.email,
+                   password: hash
+               },
+               select:{
+                   id: true,
+                   email: true,
+                   createdAt: true
+               }
+
+           })
+           //return the saved user
+
+           return newUser
+       }catch (error) {
+            if(error instanceof PrismaClientKnownRequestError){
+                if(error.code === 'P2002'){
+                    throw new ForbiddenException(
+                        'this email already exists'
+                    )
+                    // return 'this email already exists'
+                }
             }
+            throw error
+       }
 
-        })
-        //return the saved user
-
-        return newUser
     }
 
     login(){

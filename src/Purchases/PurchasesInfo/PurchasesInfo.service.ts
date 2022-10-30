3fa556164d68e6857fee13387236f0a075f0ac70
Yura <yura5742248@gmail.com>
2022-10-30 21:04:24 +0300

    fix delete purchase info

--- src/Purchases/PurchasesInfo/PurchasesInfo.service.ts
+++ src/Purchases/PurchasesInfo/PurchasesInfo.service.ts
@@ -38,16 +38,20 @@ export class PurchasesInfoService {
         const purchase = await this.userService.findUserById(purchaseInfo.user.id)
         const allPurchases = purchase.purchases
         const idPurchase = allPurchases.filter(el => el.title === purchaseInfo.title)
+        console.log(purchaseInfo)
+        console.log(allPurchases)
         const {
             id, title, price, amount, unit, unitPrice, date, image, place, user, warehouse
         } = idPurchase[0]
         const newAmount = (+amount - +purchaseInfo.amount).toString()
-        if (+newAmount <= 0) {
-           throw new Error('вы пытаетесь удалить больше чем есть материала на складе')
+        if (+newAmount === 0) {
+            await this.purchaseService.deletePurchase(id)
+        } else if (+newAmount < 0) {
+            throw new Error('вы пытаетесь удалить больше чем есть материала на складе')
         } else {
             await this.purchaseService.updatePurchase(id, title, newAmount, unit, image, place, price, date, unitPrice)
         }
-        return await this.purchasesInfoRepository.delete({id: idPurchaseInfo})
+        return await this.purchasesInfoRepository.delete({title: purchaseInfo.title})
 
     }
 
